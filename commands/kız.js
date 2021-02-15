2a1731dd70bd89247fdf8814275f123a8eeb5713
Sensei <71566572+Sensei-911@users.noreply.github.com>
2021-02-15 16:03:05 +0300

    Update kız.js

--- commands/kız.js
+++ commands/kız.js
@@ -1,3 +1,9 @@
+/*
+Bu proje exsus ve nymphdora sunucuları için özel olarak Sensei tarafından hazırlanmıştır!
+Bu proje MIT lisansı ile korunuyor ve izinsiz paylaşılması yasaktır!
+Bu proje kodun daha açıklayıcı olması için bir çok yorum eklenerek yazılmıştır!
+*/
+
 const { Discord, MessageEmbed } = require('discord.js')
 module.exports = {
 name:'kız',
